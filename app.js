8f28bedf17ba049e04ab3a68dbf5e6bcd6086007
Rchunk <rc4548301@gmail.com>
2024-08-10 16:23:27 +0530

    updated app.js

--- app.js
+++ app.js
@@ -6,16 +6,20 @@ import ReactDOM from "react-dom/client";
 //class based component-old way
 //functional component-NEW
 
-const Rahulcomponent= () => {
-    return <h1>namaste rahul functional component</h1>
+const RahulComponent= () => {
+    (
+        <div id="container">
+            <h1>namaste rahul functional component</h1>
+        </div>
+        );
 };
 
-const jsxheading = <h1 className="head">namaste react using jsx!</h1>
-console.log(jsxheading);
+//const jsxheading = <h1 className="head">namaste react using jsx!</h1>
+//console.log(jsxheading);
 
 
 const root= ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxheading);
+root.render(<RahulComponent/>);
 
 
 
